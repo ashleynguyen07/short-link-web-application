2c33bbd2103deaaacf155cd5e2b56e7b7c200759
ashleynguyen07 <nguyentramy.se@gmail.com>
2024-04-10 07:58:27 +0700

    junit test UserController class with coverage 100%

--- src/main/java/com/example/shortlinkapplication/controller/UserController.java
+++ src/main/java/com/example/shortlinkapplication/controller/UserController.java
@@ -6,13 +6,11 @@ import com.example.shortlinkapplication.entity.User;
 import com.example.shortlinkapplication.repository.UserRepository;
 import com.example.shortlinkapplication.security.CurrentUser;
 import com.example.shortlinkapplication.security.UserPrincipal;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import com.example.shortlinkapplication.service.UserService;
-import lombok.RequiredArgsConstructor;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,25 +19,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 //@RequestMapping("/profile")
 public class UserController {
 
-  @Autowired
   private static final Logger logger = LoggerFactory.getLogger(UserController.class);
   private final UserRepository userRepository;
   private final ProjectController projectController;
   private final UserService userService;
 
-  @CrossOrigin
-  @GetMapping("user/me")
-  public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
-    logger.info("User principal: {}", userPrincipal);
-    return userRepository.findById(userPrincipal.getId())
-        .orElseThrow(() -> new RuntimeException(String.valueOf(userPrincipal.getId())));
+  @Autowired
+  public UserController(UserRepository userRepository, ProjectController projectController,
+      UserService userService) {
+    this.userRepository = userRepository;
+    this.projectController = projectController;
+    this.userService = userService;
   }
 
-  @PutMapping("update-profile")
+  @CrossOrigin
+  @GetMapping("user/me")
+  public Optional<User> getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+    logger.info("User principal: {}", userPrincipal);
+    return userRepository.findById(userPrincipal.getId());
+  }
+
+  @PutMapping("/update-profile")
   public User updateUserProfile(@CurrentUser UserPrincipal userPrincipal,
       @RequestBody UpdateProfileRequest request) {
     User user = projectController.getUser(userPrincipal);
@@ -48,7 +51,7 @@ public class UserController {
     return userService.updateUserProfile(request, userID);
   }
 
-  @DeleteMapping("delete-profile")
+  @DeleteMapping("/delete-profile")
   public String deleteUserProfile(@CurrentUser UserPrincipal userPrincipal,
       @RequestBody DeleteProfileRequest request) {
     User user = projectController.getUser(userPrincipal);
