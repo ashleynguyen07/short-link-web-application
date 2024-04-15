1b13dc57bc4258dc2f0b750d4ba679e0dac1dfea
ashleynguyen07 <nguyentramy.se@gmail.com>
2024-04-15 10:04:49 +0700

    clean code

--- src/main/java/com/example/shortlinkapplication/security/oauth/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/example/shortlinkapplication/security/oauth/OAuth2AuthenticationSuccessHandler.java
@@ -1,92 +1,86 @@
 package com.example.shortlinkapplication.security.oauth;
 
+import static com.example.shortlinkapplication.security.oauth.HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
+
 import com.example.shortlinkapplication.config.AppProperties;
 import com.example.shortlinkapplication.security.TokenProvider;
-import com.example.shortlinkapplication.service.implement.JWTServiceImpl;
 import com.example.shortlinkapplication.util.CookieUtils;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.apache.coyote.BadRequestException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import org.springframework.stereotype.Component;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
-
 import java.io.IOException;
 import java.net.URI;
 import java.util.Optional;
-
-import static com.example.shortlinkapplication.security.oauth.HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
+import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-    private final TokenProvider tokenProvider;
-    private final AppProperties appProperties;
-    private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
 
-    @Autowired
-    OAuth2AuthenticationSuccessHandler(TokenProvider tokenProvider, AppProperties appProperties,
-                                       HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository) {
-        this.tokenProvider = tokenProvider;
-        this.appProperties = appProperties;
-        this.httpCookieOAuth2AuthorizationRequestRepository = httpCookieOAuth2AuthorizationRequestRepository;
+  private final TokenProvider tokenProvider;
+  private final AppProperties appProperties;
+  private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
+
+  @Autowired
+  OAuth2AuthenticationSuccessHandler(TokenProvider tokenProvider, AppProperties appProperties,
+      HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository) {
+    this.tokenProvider = tokenProvider;
+    this.appProperties = appProperties;
+    this.httpCookieOAuth2AuthorizationRequestRepository = httpCookieOAuth2AuthorizationRequestRepository;
+  }
+
+  @Override
+  public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+      Authentication authentication) throws IOException {
+    String targetUrl = determineTargetUrl(request, response, authentication);
+
+    getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    clearAuthenticationAttributes(request, response);
+  }
+
+  @Override
+  protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response,
+      Authentication authentication) {
+    Optional<String> redirectUri = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+        .map(Cookie::getValue);
+
+    if (redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
+      try {
+        throw new BadRequestException("Sorry! We've got Unauthorized Redirect URI!");
+      } catch (BadRequestException e) {
+        throw new IllegalArgumentException(e);
+      }
     }
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        System.out.println("Doing success handler....");
-        String targetUrl = determineTargetUrl(request, response, authentication);
+    String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
+    String token = tokenProvider.createToken(authentication);
 
-        getRedirectStrategy().sendRedirect(request, response, targetUrl);
-        clearAuthenticationAttributes(request, response);
-    }
+    return UriComponentsBuilder.fromUriString(targetUrl).queryParam("token", token).build()
+        .toUriString();
+  }
 
-    protected String determineTargetUrl (HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        Optional<String> redirectUri = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME).map(Cookie::getValue);
+  private boolean isAuthorizedRedirectUri(String uri) {
+    URI clientRedirectUri = URI.create(uri);
 
-        if (redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
-            try {
-                throw new BadRequestException("Sorry! We've got Unauthorized Redirect URI!");
-            } catch (BadRequestException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
-        String token = tokenProvider.createToken(authentication);
-        System.out.println("Token: " + token);
-        System.out.println("Redirect uri: " + redirectUri);
-        System.out.println("Target uri: " + targetUrl);
+    return appProperties.getOAuth2().getAuthorizedRedirectUri()
+        .stream()
+        .anyMatch(authorizedRedirectUri -> {
+          // Only validate host and port. Let the clients use different paths if they want to
+          URI authorizedURI = URI.create(authorizedRedirectUri);
 
-        return UriComponentsBuilder.fromUriString(targetUrl).queryParam("token", token).build().toUriString();
-    }
+          return authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
+              && authorizedURI.getPort() == clientRedirectUri.getPort();
+        });
 
-    private boolean isAuthorizedRedirectUri(String uri) {
-        URI clientRedirectUri = URI.create(uri);
+  }
 
-        return appProperties.getOAuth2().getAuthorizedRedirectUri()
-                .stream()
-                .anyMatch(authorizedRedirectUri -> {
-                    // Only validate host and port. Let the clients use different paths if they want to
-                    URI authorizedURI = URI.create(authorizedRedirectUri);
-                    System.out.println("authorizedURI: " + authorizedURI);
-                    if(authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
-                            && authorizedURI.getPort() == clientRedirectUri.getPort()) {
-                        System.out.println("Authorized!");
-                        return true;
-                    }
-                    return false;
-                });
-
-    }
-
-    private void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
-        super.clearAuthenticationAttributes(request);
-        httpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequest(request, response);
-    }
+  private void clearAuthenticationAttributes(HttpServletRequest request,
+      HttpServletResponse response) {
+    super.clearAuthenticationAttributes(request);
+    httpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequest(request, response);
+  }
 
 }
