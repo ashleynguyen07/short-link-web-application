a4c1e761ee203e8764a44967c024f905d5db40b6
ashleynguyen07 <nguyentramy.se@gmail.com>
2024-04-15 15:31:44 +0700

    update @Data to @Getter @Setter

--- src/main/java/com/example/shortlinkapplication/dto/url/URLRequest.java
+++ src/main/java/com/example/shortlinkapplication/dto/url/URLRequest.java
@@ -1,8 +1,10 @@
 package com.example.shortlinkapplication.dto.url;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class URLRequest {
 
   private Integer projectID;
